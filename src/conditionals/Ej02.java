9b5e03d84ac4823e31d7d4ab65355f29c19f0754
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-16 17:19:17 +0100

    Ej02a acabado

--- src/conditionals/Ej02.java
+++ src/conditionals/Ej02.java
@@ -5,6 +5,8 @@
  */
 package conditionals;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Brais
@@ -15,7 +17,23 @@ public class Ej02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        double a, b, r;
+
+        System.out.println("Introduce 2 numeros con decimales");
+        a = teclado.nextDouble();
+        b = teclado.nextDouble();
+
+        /*
+        a) este codigo evita que se haga una division entre 0 
+        consiguiendo que el programa no crashee
+         */
+        if (b != 0 && a / b == 0) {
+            r = a / b;
+            System.out.println(r);
+        }
+        
+
     }
-    
+
 }
